ec4832cd2151f0d79f8c6964def082d184d33756
Przemek <przemek@LinuxPluss-Mac.local>
2019-02-23 22:34:54 +0100

    Initial Commit

---
+++ Shopping List Realm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shopping List Realm
+//
+//  Created by Przemek on 2/23/19.
+//  Copyright © 2019 Przemek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
